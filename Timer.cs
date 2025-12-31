10334f1779522eb1993f96a2fd1bcf699bcb83d5
ItsCrist1 <ItsCristiii@gmail.com>
2025-12-31 12:39:12 +0200

    refactor, moved `Timer` (and `TimerConfig` outside of `TimerManager`

--- Timer.cs
+++ Timer.cs
@@ -1,15 +1,111 @@
-#if TOOLS
+using System;
+using System.Collections.Generic;
+
 using Godot;
 
-[Tool]
-public partial class Timer : EditorPlugin {
-	const string SINGLETON_NAME = "TimerManager";
-    const string SINGLETON_PATH = "res://addons/timer/TimerManager.cs";
-
-	public override void _EnterTree()
-		=> AddAutoloadSingleton(SINGLETON_NAME, SINGLETON_PATH);
-
-	public override void _ExitTree()
-		=> RemoveAutoloadSingleton(SINGLETON_NAME);
+public struct TimerConfig {
+	public float MaxTime { get; set; }
+	public bool AutoRefresh { get; set; }
+	public bool AutoStart { get; set; }
+	public bool TickOnPause { get; set; }
+	public bool DisposeOnTimeout { get; set; }
 }
-#endif
+
+public class Timer : IDisposable {
+	const string GENERIC_ERROR_TIMER = "Failed to create `Timer`";
+
+	LinkedListNode<WeakReference<Timer>> timerListRef;
+
+	TimerConfig Config;
+	public event Action OnStart, OnTick, Timeout, OnStop;
+	public float Time { private get; set; }
+
+	bool isPaused, isDisposed;
+
+	internal Timer(TimerConfig Config = default) {
+		this.Config = Config;
+
+		if(Config.MaxTime < 0f) {
+			GD.PushError($"{GENERIC_ERROR_TIMER}:\nMaxTime is negative");
+
+			isDisposed = true;
+			return;
+		}
+
+		if(Config.AutoStart) Time = Config.MaxTime;
+
+		timerListRef = TimerManager.RegisterTimer(new (this));
+	}
+
+	~Timer() => Dispose();
+
+	internal void Tick(float dt) {
+		if(isDisposed || isPaused || Time <= 0f
+		||(!Config.TickOnPause && Engine.TimeScale == 0f))
+			return;
+
+		Time -= dt;
+		OnTick?.Invoke();
+
+		if(Time <= 0f) {
+			Timeout?.Invoke();
+			
+			if(Config.AutoRefresh) Time += Config.MaxTime;
+			if(Config.DisposeOnTimeout) Dispose();
+		}
+	}
+
+	public void Start(float maxTime = float.NaN) {
+		if(isDisposed) return;
+
+		OnStart?.Invoke();
+
+		if(maxTime == 0f) {
+			Timeout?.Invoke();
+			if (Config.DisposeOnTimeout) Dispose();
+			return;
+		}
+
+		maxTime = float.IsNaN(maxTime) ? Config.MaxTime : maxTime;
+
+		Config.MaxTime = Time = maxTime;
+
+		Resume();
+	}
+
+	public void Pause() {
+		if(isDisposed) return;
+		isPaused = true;
+	}
+
+	public void Resume() {
+		if(isDisposed) return;
+		isPaused = false;
+	}
+
+	public void Stop() {
+		if(isDisposed) return;
+
+		OnStop?.Invoke();
+
+		Time = 0f;
+		Resume();
+	}
+
+	public bool IsGoing
+		=> !isPaused && Time > 0f;
+
+	public bool HasFinished
+		=> !isPaused && Time <= 0f;
+
+	public void Dispose() {
+		if(isDisposed) return;
+		isDisposed = true;
+
+		if(timerListRef.List != null)
+			timerListRef.List.Remove(timerListRef);
+
+		OnStart = OnTick = Timeout = OnStop = null;
+		GC.SuppressFinalize(this);
+	}
+}
--- TimerManager.cs
+++ TimerManager.cs
@@ -10,6 +10,9 @@ public partial class TimerManager : Node {
 
 	static LinkedList<WeakReference<Timer>> PendingTimers = new();
 	LinkedList<WeakReference<Timer>> Timers = new();
+
+	public static LinkedListNode<WeakReference<Timer>> RegisterTimer(WeakReference<Timer> timerWeakRef)
+		=> Instance == null ? PendingTimers.AddLast(timerWeakRef) : Instance.Timers.AddLast(timerWeakRef);
 	
 	public override void _EnterTree() {
 		Instance = this;
@@ -53,14 +56,6 @@ public partial class TimerManager : Node {
 		Instance = null;
 	}
 
-	public struct TimerConfig {
-		public float MaxTime { get; set; }
-		public bool AutoRefresh { get; set; }
-		public bool AutoStart { get; set; }
-		public bool TickOnPause { get; set; }
-		public bool DisposeOnTimeout { get; set; }
-	}
-
 	public static Timer Create(TimerConfig Config = default)
 		=> new (Config);
 
@@ -81,106 +76,5 @@ public partial class TimerManager : Node {
 		timer.Timeout += onTimeout;
 
 		return timer;
-	}
-
-	public class Timer : IDisposable {
-		const string GENERIC_ERROR_TIMER = "Failed to create `Timer`";
-
-		LinkedListNode<WeakReference<Timer>> timerListRef;
-
-		TimerConfig Config;
-		public event Action OnStart, OnTick, Timeout, OnStop;
-		public float Time { private get; set; }
-
-		bool isPaused, isDisposed;
-
-		internal Timer (TimerConfig Config = default) {
-			this.Config = Config;
-
-			if(Config.MaxTime < 0f) {
-				GD.PushError($"{GENERIC_ERROR_TIMER}:\nMaxTime is negative");
-
-				isDisposed = true;
-				return;
-			}
-			
-			if(Config.AutoStart) Time = Config.MaxTime;
-
-			WeakReference<Timer> timerWeakRef = new (this);
-
-			if(Instance == null) timerListRef = PendingTimers.AddLast(timerWeakRef);
-			else timerListRef = Instance.Timers.AddLast(timerWeakRef);
-		}
-
-		~Timer() => Dispose();
-
-		internal void Tick(float dt) {
-			if(isDisposed || isPaused || Time <= 0f 
-			|| (!Config.TickOnPause && Engine.TimeScale == 0f))
-				return;
-
-			Time -= dt;
-			OnTick?.Invoke();
-
-			if(Time <= 0f) {
-				Timeout?.Invoke();
-				if(Config.AutoRefresh) Time += Config.MaxTime;
-				if(Config.DisposeOnTimeout) Dispose();
-			}
-		}
-
-		public void Start(float maxTime=float.NaN) {
-			if(isDisposed) return;
-
-			OnStart?.Invoke();
-
-			if(maxTime == 0f) {
-				Timeout?.Invoke();
-				if(Config.DisposeOnTimeout) Dispose();
-				return;
-			}
-
-			maxTime = float.IsNaN(maxTime) ? Config.MaxTime : maxTime;
-			
-			Config.MaxTime = Time = maxTime;
-			
-			Resume();
-		}
-		
-		public void Pause() {
-			if(isDisposed) return;
-			isPaused = true;
-		}
-
-		public void Resume() {
-			if(isDisposed) return;
-			isPaused = false;
-		}
-
-		public void Stop() {
-			if(isDisposed) return;
-
-			OnStop?.Invoke();
-
-			Time = 0f;
-			Resume();
-		}
-
-		public bool IsGoing
-			=> !isPaused && Time > 0f;
-
-		public bool HasFinished
-			=> !isPaused && Time <= 0f;
-
-		public void Dispose() {
-			if(isDisposed) return;
-			isDisposed = true;
-
-			if(timerListRef.List != null)
-				timerListRef.List.Remove(timerListRef);
-
-			OnStart = OnTick = Timeout = OnStop = null;
-			GC.SuppressFinalize(this);
-		}
 	}
 }
